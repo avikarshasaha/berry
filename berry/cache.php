d6199468801ea9cbd808d34e1b99770c81591599
lexazloy <lexazloy@gmail.com>
2011-09-09 23:30:29 +0400

    *

--- berry/cache.php
+++ berry/cache.php
@@ -21,7 +21,7 @@ class Cache {
 
             self::$data = include $file;
         }
-        
+
         if ($file = self::exists($key))
             self::$scope[$key] = self::$file = $file;
         else
@@ -48,7 +48,7 @@ class Cache {
 
             self::$data = include $file;
         }
-        
+
         if ($file = self::exists($key))
             self::$scope[$key] = self::$file = $file;
         else
@@ -63,7 +63,8 @@ class Cache {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-    static function set($value, $tags = array()){        $file = end(self::$scope);
+    static function set($value, $tags = array()){
+        $file = end(self::$scope);
         $name = str_replace(file::path('tmp/'), '', $file);
         $tags = array_merge((isset(self::$data[$name]['tags']) ? self::$data[$name]['tags'] : array()), $tags);
         self::$data[$name] = array(
@@ -83,20 +84,25 @@ class Cache {
 ////////////////////////////////////////////////////////////////////////////////
 
     static function remove($key){
-        if (!is_array($key)){
-            if ($file = self::exists($key))
-                return unlink($file);
-
-            return;
-        }
-
         $result = array();
 
-        foreach (self::$data as $k => $v)
-            if (array_intersect($v, $key) and is_file($v['file'])){
-                $result[$k] = unlink($v['file']);
-                unset(self::$data[$k]);
-            }
+        if (is_array($key)){
+            foreach (self::$data as $k => $v)
+                if (array_intersect($v['tags'], $key)){
+                    $result[$k] = (is_file($v['file']) ? unlink($v['file']) : true);
+                    unset(self::$data[$k]);
+                }
+        } else {
+            $result = true;
+
+            if (!self::$data[$key])
+                return;
+
+            if ($file = self::$data[$key]['file'])
+                $result = unlink($file);
+
+            unset(self::$data[$key]);
+        }
 
         arr::export(file::path('tmp/').'cache.php', self::$data);
         return $result;
@@ -126,7 +132,9 @@ class Cache {
                 if ($k == 'file' and file_exists($v))
                     $result[] = (filemtime($v) > $mtime);
 
-                if ($k == 'db'){                    if (is_object($v)){                        $class = get_class($v);
+                if ($k == 'db'){
+                    if (is_object($v)){
+                        $class = get_class($v);
                         $vars = get_class_vars($class);
                         $v = ($vars['table'] ? $vars['table'] : inflector::tableize($class));
                     }
@@ -134,7 +142,8 @@ class Cache {
                     if ($pos = strpos($v, '.')){
                         $tmp = substr($v, 0, $pos);
                         $v = substr($v, ($pos + 1));
-                    } else {                        $tmp = SQL::SKIP;
+                    } else {
+                        $tmp = SQL::SKIP;
                     }
 
                     $query = sql::query('show table status { from ?_ } like "?_"', $tmp, $v)->fetch_row();
