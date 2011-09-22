4547e87cf16d23e97a4186d2ba24b2a40d1a3299
lexazloy <lexazloy@gmail.com>
2011-09-22 13:05:29 +0400

    * Я хз как через SQL_element нормально изменять родителя. Если есть with() всё идёт по пизде.

--- berry/sql/element.php
+++ berry/sql/element.php
@@ -90,16 +90,13 @@ class SQL_Element extends SQL_Vars implements Countable {
         if (!isset($this->scope[$name]) and $this->o->relations[$name])
             $this->scope[$name] = $this->o->_object($name);
 
+        if (is_int($name))
+            $this->o->values = $this->_scope[$name];
+
         if (is_array($this->scope[$name])){
             $this->scope[$name] = new self($this->scope[$name]);
             $this->scope[$name]->o = $this->o;
-
-            if ($this->o->relations[$name])
-                $this->scope[$name]->o = self::table($name);
         }
-
-        if (is_int($name))
-            $this->o->values = $this->_scope[$name];
 
         return $this->scope[$name];
     }
